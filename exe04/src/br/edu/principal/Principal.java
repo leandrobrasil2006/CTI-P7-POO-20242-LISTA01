f1e177d74c61fb77262680077828360c9ae40112
Leandro Brasil <leandro.brasil09@aluno.ifce.edu.br>
2024-11-05 09:58:32 -0300

    Add files via upload

---
+++ exe04/src/br/edu/principal/Principal.java
@@ -0,0 +1,16 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+    int Sal, Novosal;
+		
+		Sal = 1560;
+		Novosal = Sal + Sal * 25/100;
+		
+		System.out.println("Salario ira ser: " + Novosal);
+
+
+	}
+
+}
